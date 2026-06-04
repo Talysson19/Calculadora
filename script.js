8db1e20ccb21f279752b0b2e6fef35b5485fe583
Talysson19 <mouratalysson3@gmail.com>
2026-06-04 10:12:58 -0300

    Praticando Calculadora

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function soma(){
+    let valorN = parseFloat(document.querySelector("#valor1").value);
+    let valorN2 = parseFloat(document.querySelector("#valor2").value);
+    mostrarResultado(valorN + valorN2);
+}
+
+function subtracao(){
+    let valorN = parseFloat(document.querySelector("#valor1").value);
+    let valorN2 = parseFloat(document.querySelector("#valor2").value);
+    mostrarResultado(valorN - valorN2);
+}
+
+function multiplicacao(){
+    let valorN = parseFloat(document.querySelector("#valor1").value);
+    let valorN2 = parseFloat(document.querySelector("#valor2").value);
+    mostrarResultado(valorN * valorN2);
+}
+
+function divisao(){
+    let valorN = parseFloat(document.querySelector("#valor1").value);
+    let valorN2 = parseFloat(document.querySelector("#valor2").value);
+
+    if (valorN2 == 0) {
+        mostrarResultado("Erro: Divisão por zero");
+        return;
+    }
+
+    mostrarResultado(valorN / valorN2);
+}
+
+function mostrarResultado(valor){
+    document.querySelector("#resultado").innerHTML = "Resultado: " + valor;
+}
